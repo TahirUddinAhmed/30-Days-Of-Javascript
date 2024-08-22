2fa8d08ba6cebd369064af38930b98fdb30736c2
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-08-22 08:21:53 +0530

    Day 13 - completed

---
+++ Day 13 - Modules/Learn/Combine.js
@@ -0,0 +1,3 @@
+export * as calc from './calc.js';
+export {default as myName} from './whoami.js';
+
---
+++ Day 13 - Modules/modules/task1.js
@@ -0,0 +1,7 @@
+// Task 1: write a function that adds two numbers
+
+const add = (a, b) => {
+    return a + b;
+}
+
+export { add };
